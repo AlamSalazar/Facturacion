3fc3f84c80d88c4eff427298472ad51e74d3bf6e
AlamSalazar <56283164+AlamSalazar@users.noreply.github.com>
2023-05-13 01:13:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,23 +7,27 @@ document.getElementById("btn").onclick = function () {
     var cosVar = parseFloat(document.getElementById("cosVar").value);//Costos Varios
     var cosMed = parseFloat(document.getElementById("cosMed").value);//Costo de Medicamentos
     var posDed = parseFloat(document.getElementById("posDed").value);//Posibles deducciones
-    var Hosp = cosDia * numDia;
-    var totalS = cosCon + Hosp + cosVar + cosMed; //total SIN deducciones   tr
-    var totalC = totalS - posDed;                 //total CON deducciones
+    if (nombre != "" && !isNaN(cosCon) && !isNaN(cosDia) && !isNaN(numDia) && !isNaN(cosVar) && !isNaN(cosMed) && !isNaN(posDed)) {
+        var Hosp = cosDia * numDia;
+        var totalS = cosCon + Hosp + cosVar + cosMed; //total SIN deducciones   tr
+        var totalC = totalS - posDed;                 //total CON deducciones
 
-    document.getElementById("TODO").innerHTML = "<p class='center'><img src='logo.png' class='img2'></p>";
+        document.getElementById("TODO").innerHTML = "<p class='center'><img src='logo.png' class='img2'></p>";
 
-    table = document.createElement("table");
-    tabla("Paciente",nombre);
-    tabla("Costo de consulta","$"+cosCon);
-    tabla("Hospitalización por " + numDia + " días", "$"+Hosp);
-    tabla("Gastos varios", "$"+cosVar);
-    tabla("Medicamentos","$"+cosMed);
-    tabla("Total","$"+totalS);
-    tabla("<h3>Total real</h3>(con deducibles incluidos)", "<h2>$"+totalC +"</h2>");
-    document.getElementById("reload").innerHTML = "<button onclick='location.reload()' class='btn2'>regresar</button>";
+        table = document.createElement("table");
+        tabla("Paciente", nombre);
+        tabla("Costo de consulta", "$" + cosCon);
+        tabla("Hospitalización por " + numDia + " días", "$" + Hosp);
+        tabla("Gastos varios", "$" + cosVar);
+        tabla("Medicamentos", "$" + cosMed);
+        tabla("Total", "$" + totalS);
+        tabla("<h3>Total real</h3>(con deducibles incluidos)", "<h2>$" + totalC + "</h2>");
+        document.getElementById("reload").innerHTML = "<button onclick='location.reload()' class='btn2'>regresar</button>";
+    }
+    else { alert("Ingresa los datos solicitados"); }
+
 }
-function tabla(a,b){
+function tabla(a, b) {
     var row = table.insertRow();
     var cell1 = row.insertCell();
     var cell2 = row.insertCell();
